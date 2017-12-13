c4832ad5b8f98df7adc060da1915c2416270d235
ziya42005 <natea.wallace@gmail.com>
2017-12-13 10:34:58 -0500

    Update header.jsx

--- header.jsx
+++ header.jsx
@@ -1,1 +1,19 @@
+import React {Component} from 'react';
+import logo from '.logo.svg';
+import './App.css';
 
+class Header extends React.Component {
+  
+  render () {
+    <div className="Header">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        
+        <h1 className="App-title">
+          Books By Teachers </h1>
+      </header>
+      </div>)
+  }
+}
+
+export default Header;
